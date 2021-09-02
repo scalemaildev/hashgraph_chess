b2f00603e2d9241a06987d50cad5b57ddb614a91
scalemaildev <scale@scalemail.com>
2021-09-02 13:32:10 -0500

    redo init screen for loading

--- app/components/StartPanel.vue
+++ app/components/StartPanel.vue
@@ -16,31 +16,57 @@
       <h3>To begin, enter your Testnet account information below.</h3>
     </v-col>
   </v-row>
-  <v-row>
-    <v-spacer />
-    <v-col cols="8" align="center" justify="center">
-      <v-text-field
-	label="Account ID"/>
-    </v-col>    
-    <v-spacer />
-  </v-row>
-  <v-row>
-    <v-spacer />
-    <v-col cols="8" align="center" justify="center">
-      <v-text-field
-	label="Private Key"/>
-    </v-col>
-    <v-spacer />
-  </v-row>
-  <v-row>
-    <v-spacer />
-    <v-col cols="8" align="center" justify="center">
-      <v-btn>
-	Initialize Client
-      </v-btn>
-    </v-col>
-    <v-spacer />
-  </v-row>
+  <div v-if="!clientSet && !clientInitializing">
+    <v-row>
+      <v-spacer />
+      <v-col cols="8" align="center" justify="center">
+	<v-text-field
+	  v-model="accountId"
+	  :rules="[rules.accountId]"
+	  label="Account ID"/>
+      </v-col>    
+      <v-spacer />
+    </v-row>
+    <v-row>
+      <v-spacer />
+      <v-col cols="8" align="center" justify="center">
+	<v-text-field
+	  v-model="privateKey"
+	  type="password"
+	  label="Private Key"/>
+      </v-col>
+      <v-spacer />
+    </v-row>
+    <v-row>
+      <v-spacer />
+      <v-col cols="8" align="center" justify="center">
+	<v-btn>
+	  Initialize Client
+	</v-btn>
+      </v-col>
+      <v-spacer />
+    </v-row>
+    <div v-if="clientError">
+      <br>
+      <v-row align="center" justify="center">
+	<v-col cols="12" align="center" justify="center">
+	  <span style="color: red;"><h3>An error occurred initializing the client. Check the console log for details.</h3></span>
+	</v-col>
+      </v-row>
+    </div>
+  </div>
+  <div v-if="!clientSet && clientInitializing">
+    <v-row>
+      <v-col cols="12" align="center" justify="center">
+	<v-progress-circular
+	  indeterminate
+	  />
+      </v-col>
+      <v-col cols="12" align="center" justify="center">
+	... Initializing Hedera Client ...
+      </v-col>
+    </v-row>
+  </div>
 </v-container>  
 </template>
 
@@ -48,6 +74,21 @@
 import { mapMutations } from 'vuex';
 
 export default {
+    data () {
+	return {
+	    accountId: "",
+	    privateKey: "",
+	    clientInitializing: false,
+	    clientSet: false,
+	    clientError: false,
+	    rules: {
+		accountId: value => {
+		    const pattern = /0.0.[0-9]{3,}/
+		    return pattern.test(value) || "Account ID should look like 0.0.xxx"
+		}
+	    }
+	}
+    },
     methods: {
 	...mapMutations([
 	    'setActivePanel'
