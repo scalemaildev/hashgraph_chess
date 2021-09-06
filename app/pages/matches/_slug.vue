3b5c603e8962efc1206746f298d4df64174bc428
scalemaildev <scale@scalemail.com>
2021-09-06 18:40:16 -0500

    skeleton for dynamic pages

---
+++ app/pages/matches/_slug.vue
@@ -0,0 +1,24 @@
+<template>
+<v-container fluid class="content-body">
+  <v-row>
+    <v-col cols="12" align="center" justify="center">
+      <h2>Match ID: {{ this.topicId }}</h2>
+    </v-col>
+  </v-row>
+</v-container>
+</template>
+
+<script>
+export default {
+    async asyncData({ params }) {
+	const topicId = params.slug;
+	return { topicId }
+    },
+    
+    computed: {
+    },
+    
+    methods: {
+    },
+}
+</script>
