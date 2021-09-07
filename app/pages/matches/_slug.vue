b351674abc2a1383d9e559823bffa74b84b60243
scalemaildev <scale@scalemail.com>
2021-09-07 08:27:52 -0500

    skeleton for match page

--- app/pages/matches/_slug.vue
+++ app/pages/matches/_slug.vue
@@ -1,11 +1,25 @@
 <template>
-<v-container fluid class="content-body">
-  <v-row>
-    <v-col cols="12" align="center" justify="center">
-      <h2>Match ID: {{ this.topicId }}</h2>
-    </v-col>
-  </v-row>
-</v-container>
+  <div>
+    <v-row>
+      <v-col cols="12" align="center" justify="center">
+	<v-container fluid class="match-header">
+	  <h1>Match ID: {{ this.topicId }}</h1>
+	</v-container>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col xs="12" sm="12" md="6" lg="6" xl="6" align="center" justify="center">
+	<v-container fluid class="content-body-left">
+	  <h2>Chess Game</h2>
+	</v-container>
+      </v-col>
+      <v-col xs="12" sm="12" md="6" lg="6" xl="6">
+	<v-container fluid class="content-body-right">
+	  <h2>Chatbox</h2>
+	</v-container>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
