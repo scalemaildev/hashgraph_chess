c14d7713d58d6d29cba2e90e10d527917ac84f6e
scalemaildev <scale@scalemail.com>
2021-09-25 13:10:06 -0500

    moved chess board to a new component

---
+++ app/components/ChessBoard.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-container>
+  <v-row no-gutters align="center" class="flex-column d-flex">
+    <nuxt-img src="/game/border_top.png" :style="horzFrameSize"/>
+    <div class="d-flex" style="position: relative">
+      <nuxt-img src="/game/border_left_legend.png"
+                :style="vertFrameSize"/>
+      <div v-for="col in 8" :key="col">
+        <div v-for="row in 8" :key="row">
+          <div :style="getTileBg(row, col)">
+            <nuxt-img :src="getTile(row, col)"
+                      :style="pieceStyle"/>
+          </div>
+        </div>  
+      </div>
+      <img src="/game/border_right.png"
+           :style="vertFrameSize">
+    </div>
+    <img src="/game/border_bottom_legend.png"
+         :style="horzFrameSize">
+  </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+    props: ['boardState'],
+    
+    computed: {
+        pieceStyle() {
+            let edge = this.getTileEdge();
+            
+            return { width: edge, height: edge };
+        },
+        horzFrameSize() {
+            switch (this.$vuetify.breakpoint.name) {
+            case 'xs': return { width: '340px', height: '10px' }
+            case 'sm': return { width: '384px', height: '14px' }
+            case 'md': return { width: '340px', height: '10px' }
+            case 'lg': return { width: '384px', height: '14px' }
+            case 'xl': return { width: '510px', height: '16px' }
+            }
+        },
+        vertFrameSize() {
+            switch (this.$vuetify.breakpoint.name) {
+            case 'xs': return { width: '10px', height: '320px' }
+            case 'sm': return { width: '12px', height: '360px' }
+            case 'md': return { width: '10px', height: '320px' }
+            case 'lg': return { width: '12px', height: '360px' }
+            case 'xl': return { width: '15px', height: '480px' }
+            }
+        },
+    },
+    
+    methods: {
+        getTile(row, col) {
+            let piece = this.boardState[row - 1][col - 1];
+            return `/game/${piece}.png`;
+        },
+        getTileBg(row, col) {
+            let bg = (col + row) % 2 === 0 ? "url('/game/b.png')" : "url('/game/g.png')";
+            let edge = this.getTileEdge();
+            
+            return { width: edge, height: edge, background: bg };
+        },
+        getTileEdge() {
+            switch (this.$vuetify.breakpoint.name) {
+            case 'xs': return '40px'
+            case 'sm': return '45px'
+            case 'md': return '40px'
+            case 'lg': return '45px'
+            case 'xl': return '60px'
+            }
+        },
+    }
+}
+</script>
--- app/components/GamePanel.vue
+++ app/components/GamePanel.vue
@@ -1,34 +1,14 @@
 <template>
 <v-container fluid class="gamePanel-wrapper">
-  <v-row no-gutters align="center" class="flex-column d-flex">
-    <nuxt-img src="/game/border_top.png" :style="horzFrameSize"/>
-    <div class="d-flex" style="position: relative">
-      <nuxt-img src="/game/border_left_legend.png"
-                :style="vertFrameSize"/>
-      <div v-for="col in 8" :key="col">
-        <div v-for="row in 8" :key="row">
-          <div :style="getTileBg(row, col)">
-            <nuxt-img :src="getTile(row, col)"
-                      :style="pieceStyle"/>
-          </div>
-        </div>  
-      </div>
-      <img src="/game/border_right.png"
-           :style="vertFrameSize">
-    </div>
-    <img src="/game/border_bottom_legend.png"
-         :style="horzFrameSize">
-  </v-row>
+  <ChessBoard
+    :boardState="MATCH_BOARD_STATE(this.topicId)"/>
   <v-row align="center" justify="center">
-    
     <v-col cols="12" align="center">
       <h4>Turn Info and Scroll Buttons Here</h4>
     </v-col>
-    
     <v-col cols="12" align="center">
       <h4>{{ turnStatus() }}</h4>
     </v-col>
-    
     <div v-show="userType != 'o' && userType == turn">
       <div v-if="!SUBMITTING_MOVE">
         <v-col cols="12" align="center">
@@ -138,31 +118,6 @@ export default {
             return errors;
         },
         
-        /* CSS Styling */
-        pieceStyle() {
-            let edge = this.getTileEdge();
-            
-            return { width: edge, height: edge };
-        },
-        horzFrameSize() {
-            switch (this.$vuetify.breakpoint.name) {
-            case 'xs': return { width: '340px', height: '10px' }
-            case 'sm': return { width: '384px', height: '14px' }
-            case 'md': return { width: '340px', height: '10px' }
-            case 'lg': return { width: '384px', height: '14px' }
-            case 'xl': return { width: '510px', height: '16px' }
-            }
-        },
-        vertFrameSize() {
-            switch (this.$vuetify.breakpoint.name) {
-            case 'xs': return { width: '10px', height: '320px' }
-            case 'sm': return { width: '12px', height: '360px' }
-            case 'md': return { width: '10px', height: '320px' }
-            case 'lg': return { width: '12px', height: '360px' }
-            case 'xl': return { width: '15px', height: '480px' }
-            }
-        },
-        
         /* Data */
         matchPgn () {
             return this.MATCH_PGN_LATEST(this.topicId);
@@ -194,27 +149,6 @@ export default {
                                            'CREATE_GAME',
                                            'LOAD_PGN']),
         ...mapActions('sessionStorage', ['SEND_MESSAGE']),
-        
-        /* Styles */
-        getTile(row, col) {
-            let piece = this.MATCH_BOARD_STATE(this.topicId)[row - 1][col - 1];
-            return `/game/${piece}.png`;
-        },
-        getTileBg(row, col) {
-            let bg = (col + row) % 2 === 0 ? "url('/game/b.png')" : "url('/game/g.png')";
-            let edge = this.getTileEdge();
-            
-            return { width: edge, height: edge, background: bg };
-        },
-        getTileEdge() {
-            switch (this.$vuetify.breakpoint.name) {
-            case 'xs': return '40px'
-            case 'sm': return '45px'
-            case 'md': return '40px'
-            case 'lg': return '45px'
-            case 'xl': return '60px'
-            }
-        },
         
         /* Setup */
         initTranslatedGameState() {
