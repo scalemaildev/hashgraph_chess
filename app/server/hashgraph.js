712c560fb29e13afb74cb5aad3ff1876ec9da2ae
scalemaildev <scale@scalemail.com>
2021-10-06 10:14:21 -0500

    Revert "sub problem remains"
    This reverts commit 0b9f7a6a23a56ec98fe451430401cf10e036349e.

--- app/server/hashgraph.js
+++ app/server/hashgraph.js
@@ -4,100 +4,28 @@ const TextDecoder = require("text-encoding").TextDecoder;
 /* Hashgraph SDK */
 const {
     Client,
-    AccountId,
-    PrivateKey,
     TopicId,
-    TopicCreateTransaction,
     TopicMessageQuery,
-    TopicMessageSubmitTransaction,
 } = require("@hashgraph/sdk");
 
-var HederaClient;
-var subscriptions = {};
+var serverClient;
 
-function initHashgraphClient(newAccountId, newPrivateKey) {
-    try {
-        HederaClient = Client.forTestnet(); // Testnet only as for right now. Can add Mainnet in prod
-
-        // use a specific mirror node if it's defined
-        if (process.env.MIRROR_NODE_URL) {
-            console.log('Using specified mirror node: ' + process.env.MIRROR_NODE_URL);
-            HederaClient.setMirrorNetwork(process.env.MIRROR_NODE_URL);
-        } else {
-            console.log('Using default mirror node');
-        }
-        
-        let accountId = AccountId.fromString(newAccountId);
-        let privateKey = PrivateKey.fromString(newPrivateKey);
-        HederaClient.setOperator(accountId, privateKey);
-        return {
-            success: true,
-            responseMessage: 'Hedera Hashgraph client initialized'
-        };
-    } catch (error) {
-        return {
-            success: false,
-            responseMessage: 'Hedera Hashgraph client failed to initialize'
-        };
-    }
-}
-
-function unsetClient() {
-    try {
-        HederaClient = null;
-        return {
-            success: true,
-            responseMessage: 'Hedera Hashgraph client has been unset'
-        };
-    } catch (error) {
-        return {
-            success: false,
-            responseMessage: 'Failed to unset the Hedera Hashgraph client somehow'
-        };
-    }
-}
-
-async function createNewTopic() {
-    try {
-        const tx = await new TopicCreateTransaction().execute(HederaClient);
-        const topicReceipt = await tx.getReceipt(HederaClient);
-        const newTopicId = topicReceipt.topicId.toString();
-        return {
-            success: true,
-            responseMessage: 'Created new topic ' + newTopicId,
-            newTopicId: newTopicId,
-        };
-    } catch (error) {
-        return {
-            success: false,
-            responseMessage: 'Failed to create a new topic',
-            errorMessage: error
-        };
-    }
-}
-
-async function sendHCSMessage(data) {
-    let messageObject = data.context;
-    messageObject['operator'] = data.operator;
-    let messagePayload = JSON.stringify(messageObject);
+if (!process.env.SERVER_CLIENT_ID || !process.env.SERVER_CLIENT_KEY) {
+    console.warn('No info found for server-side hashgraph client!');
+} else {
+    // Testnet only as for right now. Can add Mainnet in prod
+    serverClient = Client.forTestnet();
+    serverClient.setOperator(process.env.SERVER_CLIENT_ID, process.env.SERVER_CLIENT_KEY);
     
-    try {
-        await new TopicMessageSubmitTransaction({
-            topicId: TopicId.fromString(data.context.topicId),
-            message: messagePayload})
-            .execute(HederaClient);
-        
-        return {
-            success: true,
-            responseMessage: 'Sent message to HCS'
-        };
-    } catch (error) {
-        return {
-            success: false,
-            responseMessage: 'Failed to send message to HCS'
-        };
+    // use a specific mirror node if it's defined
+    if (process.env.MIRROR_NODE_URL) {
+        serverClient.setMirrorNetwork(process.env.MIRROR_NODE_URL);
     }
+
+    console.log('Created server-side client...');
 }
+
+var subscriptions = {};
 
 async function subscribeToTopic(io, topicIdString) {
     const topicId = TopicId.fromString(topicIdString);
@@ -111,7 +39,7 @@ async function subscribeToTopic(io, topicIdString) {
         let sub = new TopicMessageQuery()
             .setTopicId(topicId)
             .setStartTime(0)
-            .subscribe(HederaClient, res => {
+            .subscribe(serverClient, res => {
                 let contents = new TextDecoder("utf-8").decode(res.contents);
                 io.emit('newHCSMessage', contents);
             });
@@ -129,9 +57,5 @@ async function subscribeToTopic(io, topicIdString) {
 }
 
 module.exports = {
-    initHashgraphClient,
-    unsetClient,
-    createNewTopic,
-    sendHCSMessage,
     subscribeToTopic,
 };
