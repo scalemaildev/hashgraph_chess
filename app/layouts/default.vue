257dff519d865bcd9dd31d4a9099b657c4d15fbe
scalemaildev <scale@scalemail.com>
2021-10-12 14:57:05 -0500

    testing computed styles

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
 <v-app dark>
-  <v-main class="main-layout">
+  <v-main v-bind:class="mainLayoutSize">
     <Navbar />
     <Nuxt />
   </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+    computed: {
+        mainLayoutSize() {
+            return this.$vuetify.breakpoint.xs ? "main-layout-mobile" : "main-layout"
+        }
+    }
+}
+</script>
