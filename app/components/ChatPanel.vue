c76acb1971b20f080552f620ad516290ef5c1445
scalemaildev <scale@scalemail.com>
2021-10-15 18:53:49 -0500

    fixing chat window style

--- app/components/ChatPanel.vue
+++ app/components/ChatPanel.vue
@@ -1,16 +1,10 @@
 <template>
 <v-container fluid class="content-body chat-panel">
-  <v-row style="margin-bottom: 0;">
-    <v-col align="center" style="margin-bottom: 0; padding-bottom: 0;">
-     <h3>Player Chat</h3>
-    </v-col>
-    <v-col cols="12">
-      <MessagesWindow :topicId="topicId" />
-    </v-col>
-    <v-col cols="12" v-show="userType != 'o'" style="padding-bottom: 0; padding-top: 0;">
-      <MessageBox :topicId="topicId" />
-    </v-col>
-  </v-row>
+  <div align="center">
+    <h3>Player Chat</h3>
+  </div>
+  <MessagesWindow :topicId="topicId" />
+  <MessageBox :topicId="topicId" />
 </v-container>
 </template>
 
