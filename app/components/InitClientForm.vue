a571dd80586530fab3ffc80f7e3e6cb65813ebec
scalemaildev <scale@scalemail.com>
2022-04-09 22:02:37 -0500

    no more init form

--- app/components/InitClientForm.vue
+++ app/components/InitClientForm.vue
@@ -1,92 +1,22 @@
 <template>
 <div>
-  <v-form
-    @submit.prevent="submit">
-    <v-row>
-      <v-spacer />
-      <v-col cols="8" align="center" justify="center">
-        <v-text-field
-          v-model="accountId"
-          :error-messages="accountIdErrors"
-          required
-          @input="$v.accountId.$touch()"
-          @blur="$v.accountId.$touch()"
-          label="Testnet Account ID"/>
-      </v-col>    
-      <v-spacer />
-    </v-row>
-    <v-row>
-      <v-spacer />
-      <v-col cols="8" align="center" justify="center">
-        <v-text-field
-          v-model="privateKey"
-          :error-messages="privateKeyErrors"
-          type="password"
-          required
-          @input="$v.privateKey.$touch()"
-          @blur="$v.privateKey.$touch()"
-          label="Testnet Account Private Key"/>
-      </v-col>
-      <v-spacer />
-    </v-row>
-    <v-row>
-      <v-spacer />
-      <v-col align="center" justify="center">
-        <v-btn
-          type="submit">
-          Initialize Client
-        </v-btn>
-      </v-col>
-      <v-spacer />
-    </v-row>    
-  </v-form>
-  <div v-if="clientError" class="content-spaced-small">
-    <v-row align="center" justify="center">
-      <v-col cols="12" align="center" justify="center">
-        <span style="color: red;"><h3>An error occurred initializing the client:</h3></span>
-        <p style="color: red;">Double check your <strong>TESTNET</strong> Account ID and Private Key.</p>
-      </v-col>
-    </v-row>
-  </div>
+  <v-row>
+    <v-col align="center">
+      <v-btn @click.prevent="initHederaClient">
+          Connect HashPack Wallet
+      </v-btn>
+    </v-col>
+  </v-row>
 </div>
 </template>
 
 <script>
 import { mapMutations, mapActions } from 'vuex';
-import { validationMixin } from 'vuelidate';
-import { required, helpers } from 'vuelidate/lib/validators';
-
-const accountIdRegex = helpers.regex('accountIdRegex', /0.0.[0-9]{3,}/);
 
 export default {
-    mixins: [validationMixin],
-    
-    validations: {
-        accountId: { required, accountIdRegex },
-        privateKey: { required },
-    },
-    
     data () {
         return {
-            accountId: "",
-            privateKey: "",
-            clientError: false,
-        }
-    },
-    
-    computed: {
-        accountIdErrors () {
-            const errors = [];
-            if (!this.$v.accountId.$dirty) return errors
-            !this.$v.accountId.required && errors.push('Account ID is required.')
-            !this.$v.accountId.accountIdRegex && errors.push('Account ID should look like 0.0.xxx.')
-            return errors;
-        },
-        privateKeyErrors () {
-            const errors = [];
-            if (!this.$v.privateKey.$dirty) return errors
-            !this.$v.privateKey.required && errors.push('Private Key is required.')
-            return errors;
+            clientError: false
         }
     },
     
@@ -96,13 +26,13 @@ export default {
                                            'SET_PRIVATE_KEY',
                                            'TOGGLE_LOCK_BUTTON']),
         ...mapActions('sessionStorage', ['INIT_HEDERA_CLIENT']),
-        submit () {
-            this.$v.$touch();
-            if (!this.$v.$invalid) {
-                this.initHederaClient();
-            }
+        
+        async initHederaClient() {
+            console.log('hi');
         },
-        async initHederaClient () {
+        
+        /*
+        async initHederaClient_OLD () {
             this.clientError = false;
             
             const response = await this.INIT_HEDERA_CLIENT({
@@ -122,6 +52,7 @@ export default {
                 this.clientError = true;
             }
         },
+        */
     }
 }
 </script>
