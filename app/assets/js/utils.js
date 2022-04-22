f72e63e9ac93a508843f15baf97275f42bab9dea
scalemaildev <scale@scalemail.com>
2022-04-22 16:03:00 -0500

    trying to resolve mainnet issues

--- app/assets/js/utils.js
+++ app/assets/js/utils.js
@@ -14,8 +14,8 @@ async function signAndMakeBytes(tx, signingAcctId) {
     const privateKey = PrivateKey.fromString(process.env.SERVER_PRIVATE_KEY);
     const publicKey = privateKey.publicKey;
     
-    let newId = TransactionId.generate(signingAcctId);
-    tx.setTransactionId(newId);
+    let transId = TransactionId.generate(signingAcctId);
+    tx.setTransactionId(transId);
 
     tx = await tx.freezeWith(HederaClient);
     let txBytes = tx.toBytes();
