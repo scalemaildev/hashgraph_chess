7d6c739cd4291c25763caff571cdfde0699ebce8
scalemaildev <scale@scalemail.com>
2022-04-22 20:30:30 -0500

    move a period

--- app/components/StartPanel.vue
+++ app/components/StartPanel.vue
@@ -7,7 +7,7 @@
   </v-row>
   <v-row>
     <v-col cols="12" align="center">
-      <p>This is an application that allows players to conduct chess matches over the <a href="https://hedera.com/consensus-service">Hedera Consensus Service.</a> Chess moves (and chat messages) are submitted to the consensus service, where they are ordered as an immutable log of events. These events are then translated into the board states and messages that are served to the client via an API call.</p>
+      <p>This is an application that allows players to conduct chess matches over the <a href="https://hedera.com/consensus-service">Hedera Consensus Service</a>. Chess moves (and chat messages) are submitted to the consensus service, where they are ordered as an immutable log of events. These events are then translated into the board states and messages that are served to the client via an API call.</p>
       <p>In order to use this application, you will need the <a href="https://www.hashpack.app/">HashPack</a> browser extension wallet, as well as a <a href="https://hedera.com/account-creation">Mainnet account</a>. Use the <b>Connect Wallet</b> button on the navbar.</p>
     </v-col>
   </v-row>
